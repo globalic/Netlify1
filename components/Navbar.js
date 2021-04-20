ec40d64c82913ce1add8a655b216b629d4108671
Benjamin <1234hteimaj4321@gmail.com>
2021-04-20 17:15:19 -0600

    http -> https

--- components/Navbar.js
+++ components/Navbar.js
@@ -33,7 +33,7 @@ export const Navbar = ({ currentPage }) => {
             <svg
               className="fill-current h-3 w-3"
               viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+              xmlns="https://www.w3.org/2000/svg"
             >
               <title>Menu</title>
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
@@ -85,7 +85,7 @@ export const Navbar = ({ currentPage }) => {
                 <span className="ml-2 font-size inline-block">
                   {" "}
                   <svg
-                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns="https://www.w3.org/2000/svg"
                     className="h-3 w-3"
                     fill="none"
                     viewBox="0 0 24 24"
@@ -151,7 +151,7 @@ export const Navbar = ({ currentPage }) => {
           <button type="submit" className="absolute right-0 top-0 mt-3 mr-12">
             <svg
               className="text-gray-600 dark:text-gray-300 h-4 w-4 fill-current"
-              xmlns="http://www.w3.org/2000/svg"
+              xmlns="https://www.w3.org/2000/svg"
               version="1.1"
               id="Capa_1"
               x="0px"
