3aa9f662a5db25676d7b28203d40525c99927e0e
quyctd <quy.dc98@gmail.com>
2021-05-06 12:50:22 +0700

    Search box mobile

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,6 +3,37 @@ import Link from "next/link";
 import { useState } from "react";
 import { DarkModeToggle } from "../components/DarkModeToggle";
 
+const SearchBox = () => {
+  return (
+    <div className="relative mx-auto mt-4 lg:mt-0 text-gray-600 dark:text-gray-300 block">
+      <input
+        className="border-2 mr-6 border-gray-300 h-10 w-full lg:w-auto pl-2 pr-8 rounded-lg text-sm focus:outline-none"
+        type="search"
+        name="search"
+        placeholder="Search"
+      />
+      <button
+        type="submit"
+        className="absolute right-0 top-0 mt-3 mr-3 lg:mr-8"
+      >
+        <svg
+          className="text-gray-600 dark:text-gray-300 h-4 w-4 fill-current"
+          xmlns="https://www.w3.org/2000/svg"
+          version="1.1"
+          id="Capa_1"
+          x="0px"
+          y="0px"
+          viewBox="0 0 56.966 56.966"
+          width="512px"
+          height="512px"
+        >
+          <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
 export const Navbar = ({ currentPage }) => {
   const [blogDropdownOpen, setblogDropdownOpen] = useState(false);
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
@@ -17,8 +48,8 @@ export const Navbar = ({ currentPage }) => {
   const onTerms = currentPage === "terms" ? true : false;
 
   return (
-    <nav className="sticky inset-0 z-20 flex items-center justify-between bg-white dark:bg-gray-900 py-4 lg:px-8 shadow border-solid border-t-2 border-blue-700">
-      <div className="flex justify-between w-full pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0 lg:w-auto lg:border-b-0">
+    <nav className="sticky inset-0 z-20 lg:flex items-center justify-between bg-white dark:bg-gray-900 py-4 lg:px-8 shadow border-solid border-t-2 border-blue-700">
+      <div className="flex justify-between w-full px-8 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0 lg:px-0 lg:w-auto lg:border-b-0">
         <div className="pl-6 lg:pl-0 flex items-center flex-shrink-0 text-gray-800 dark:text-gray-100">
           <img src="/logo.svg" height={30} width={150} />
         </div>
@@ -40,6 +71,10 @@ export const Navbar = ({ currentPage }) => {
             </svg>
           </button>
         </div>
+      </div>
+
+      <div className="px-8 lg:hidden">
+        <SearchBox />
       </div>
 
       <div
@@ -146,32 +181,10 @@ export const Navbar = ({ currentPage }) => {
         <div className="mt-4 lg:mt-0">
           <DarkModeToggle />
         </div>
-        <div className="relative mx-auto mt-4 lg:mt-0 text-gray-600 dark:text-gray-300 block">
-          <input
-            className="border-2 mr-6 border-gray-300 h-10 w-full lg:w-auto pl-2 pr-8 rounded-lg text-sm focus:outline-none"
-            type="search"
-            name="search"
-            placeholder="Search"
-          />
-          <button
-            type="submit"
-            className="absolute right-0 top-0 mt-3 mr-3 lg:mr-8"
-          >
-            <svg
-              className="text-gray-600 dark:text-gray-300 h-4 w-4 fill-current"
-              xmlns="https://www.w3.org/2000/svg"
-              version="1.1"
-              id="Capa_1"
-              x="0px"
-              y="0px"
-              viewBox="0 0 56.966 56.966"
-              width="512px"
-              height="512px"
-            >
-              <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-            </svg>
-          </button>
+        <div className="hidden lg:block">
+          <SearchBox />
         </div>
+
         <div className="text-center lg:flex">
           <Link href="/sign-in">
             <p className="block text-md px-4 py-3 rounded text-blue-700 dark:text-blue-400 font-bold hover:text-white dark:hover:text-white mt-4 hover:bg-blue-700 lg:mt-0 cursor-pointer">
